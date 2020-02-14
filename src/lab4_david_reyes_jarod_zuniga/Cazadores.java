e2c43c6d782a3b68e89741ddd7f40ab5094cdd32
David Reyes <davidreyesd13@gmail.com>
2020-02-14 16:56:45 -0600

    constructores de clases

--- src/lab4_david_reyes_jarod_zuniga/Cazadores.java
+++ src/lab4_david_reyes_jarod_zuniga/Cazadores.java
@@ -2,8 +2,9 @@ package lab4_david_reyes_jarod_zuniga;
 
 public class Cazadores extends Jugadores {
 
-    public Cazadores(String nombre, double peso, double musculatura, double reflejos, String pos) {
-        super(nombre, peso, musculatura, reflejos, pos);
+    public Cazadores(String nombre, double peso, double musculatura, double reflejos, String pos, String capitan) {
+        super(nombre, peso, musculatura, reflejos, pos, capitan);
     }
 
+    
 }
--- src/lab4_david_reyes_jarod_zuniga/Guardian.java
+++ src/lab4_david_reyes_jarod_zuniga/Guardian.java
@@ -2,10 +2,11 @@ package lab4_david_reyes_jarod_zuniga;
 
 public class Guardian extends Jugadores{
 
-    public Guardian(String nombre, double peso, double musculatura, double reflejos, String pos) {
-        super(nombre, peso, musculatura, reflejos, pos);
-        
+    public Guardian(String nombre, double peso, double musculatura, double reflejos, String pos, String capitan) {
+        super(nombre, peso, musculatura, reflejos, pos, capitan);
     }
+
+   
 
    
     
