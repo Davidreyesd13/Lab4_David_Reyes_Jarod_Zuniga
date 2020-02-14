d70befd8b860af041be676e9072d82d4817955e3
Rosalba <JJJZuniga>
2020-02-14 14:15:10 -0600

    Main

--- src/lab4_david_reyes_jarod_zuniga/Lab4_David_Reyes_Jarod_Zuniga.java
+++ src/lab4_david_reyes_jarod_zuniga/Lab4_David_Reyes_Jarod_Zuniga.java
@@ -1,9 +1,37 @@
 package lab4_david_reyes_jarod_zuniga;
 
+import java.util.Scanner;
+
 public class Lab4_David_Reyes_Jarod_Zuniga {
 
-    public static void main(String[] args) {
+    static Scanner sc = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al programa juegos de Hogwarts");
+        System.out.println("");
+        System.out.println("|-------------------------*MENU*--------------------------|");
+        System.out.println("|1.CRUD Equipo                                            |"
+                       + "\n|2.CRUD Jugadores                                         |"
+                +        "\n|3.Simulación del Juego                                   |");
+        System.out.println("|---------------------------------------------------------|");
+        int opc=sc.nextInt();
+        switch (opc) {
+            case 1:
+                System.out.println("1. agregar equipos");
+                System.out.println("2. agregar equipos");
+                System.out.println("3. agregar equipos");
+                System.out.println("4. agregar equipos");
+                
+                break;
+            case 2:
+                
+                break;
+            case 3:
+                
+                break;
+            default:
+                throw new AssertionError();
+        }
     }
-    
+
 }
